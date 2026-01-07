23835071760baa244b021e48a669aaa7521276dc
paulosplima <155822549+paulosplima@users.noreply.github.com>
2026-01-07 12:02:58 -0300

    feat: Initialize React app and set up loading state
    Updates `index.html` to explicitly load `index.tsx` as a module and enhances the loading state by centering the text and setting the correct font.
    
    Modifies `index.tsx` to handle cases where the root element is not found gracefully by logging an error instead of throwing, and ensures the React app is rendered correctly.

--- index.tsx
+++ index.tsx
@@ -5,12 +5,12 @@ import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  console.error("Could not find root element to mount to");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
